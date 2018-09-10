750f571e35d1c8c5c3a03f5136e4ebdd2c6ee0e2
huanwei <huan@harmonycloud.cn>
2018-09-10 17:52:51 +0800

    add LICENSE, fix log issue
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- cmd/rocketmq-operator/main.go
+++ cmd/rocketmq-operator/main.go
@@ -1,8 +1,30 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
+	goflag "flag"
 	"fmt"
+	"github.com/golang/glog"
+	"github.com/spf13/pflag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"os"
+
+	"k8s.io/apiserver/pkg/util/logs"
 
 	"github.com/huanwei/rocketmq-operator/cmd/rocketmq-operator/app"
 	operatoropts "github.com/huanwei/rocketmq-operator/pkg/options"
@@ -20,6 +42,20 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error reading config: %v\n", err)
 		os.Exit(1)
 	}
+
+	opts.AddFlags(pflag.CommandLine)
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	pflag.Parse()
+	goflag.CommandLine.Parse([]string{})
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	pflag.VisitAll(func(flag *pflag.Flag) {
+		glog.V(2).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
+
 	if err := app.Run(opts); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
