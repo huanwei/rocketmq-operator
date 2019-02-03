c9b1fb7f41d45f77ce6cea273ed303f072bfd06e
huanwei <huan@harmonycloud.cn>
2019-02-03 11:40:09 +0800

    delete bak
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- docker/rocketmq-bak/brokerStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./brokerGenConfig.sh
-#./mqbroker -n rocketmq-namesrv-service.rocketmq-operator:9876
-#./mqbroker -n 192.168.196.49:9876;192.168.122.177:9876
-./mqbroker -n $NAMESRV_ADDRESS
